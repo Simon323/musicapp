f434f2f270b0d94039fcd667cfa01bf963a9829e
Szymon Miękus <miekusoft@gmail.com>
2022-07-05 23:54:46 +0200

    Input search section

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,27 +8,27 @@ function Search() {
         <img
           src={require("../img/backgraphics.png")}
           alt=""
-          className="absolute top-[22rem] left-[-47rem]"
+          className="absolute top-[22rem] left-[-60rem]"
         />
         <img
           src={require("../img/d1.png")}
           alt=""
-          className="absolute w-[16rem] top-[26rem]"
+          className="w-[16rem] top-[26rem] absolute"
         />
         <img
           src={require("../img/d2.png")}
           alt=""
-          className="absolute w-[9rem] top-[32.7em] left-[7rem]"
+          className="w-[9rem] absolute top-[32.7rem] left-[7rem]]"
         />
         <img
           src={require("../img/d3.png")}
           alt=""
-          className="absolute w-[9rem] top-[32rem] left-[17rem]"
+          className="w-[9rem] top-[33rem] left-[17rem] absolute"
         />
         <img
           src={require("../img/d4.png")}
           alt=""
-          className="absolute w-[17rem] top-[50rem] left-[2rem]"
+          className="w-[17rem] top-[50rem] left-[2rem] absolute"
         />
       </div>
 
@@ -36,8 +36,13 @@ function Search() {
       <div className="right flex items-start flex-col justify-start flex-1 h-[100%] pt-[9rem]">
         {/* Search */}
         <div className="searchbar flex justify-start w-[100%]">
-          <input type="text" placeholder="Enter the keyword or URL" />
-          <div className="searchIcon">
+          <input
+            type="text"
+            placeholder="Enter the keyword or URL"
+            className="flex-[19] outline-none bg-[#020917] rounded-xl p-3 h-[3rem]"
+          />
+          {/* Search icon */}
+          <div className="searchIcon flex flow-1 items-center rounded-xl ml-4 bg-gradient-to-bl from-[#F3071D] to-[#E600FF] p-4 h-[3rem]">
             <img
               src={require("../img/search.png")}
               alt=""
@@ -45,6 +50,26 @@ function Search() {
             />
           </div>
         </div>
+        {/* tild icon */}
+        <div className="tild flex justify-start mt-7 items-center w-[100%]">
+          <img
+            src={require("../img/Path 318.png")}
+            alt=""
+            className="w-[5rem]"
+          />
+        </div>
+        {/* paragraph */}
+        <div className="detail flex flex-col mt-5 text-4xl">
+          <span>Search Music by</span>
+          <span>
+            <b>Name or Direct URL</b>
+          </span>
+          <span className="text-sm mt-3 text-[#4D586A]">
+            Duis feugiat congue metus, ultrices vulputate <br /> nibh viverra
+            eget. Vestibulum ullamcorper <br /> volutpat varius.
+          </span>
+        </div>
+        {/* music player */}
       </div>
     </div>
   );
