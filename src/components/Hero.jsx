22841b70c1b56ddfcabb5bc14608ba2f91f9d424
Szymon Miękus <miekusoft@gmail.com>
2022-07-11 22:50:29 +0200

    Hero animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,57 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 import DownloadAds from "./DownloadAds";
+import VisibilitySensor from "react-visibility-sensor";
+import { motion } from "framer-motion";
 
 function Hero(props) {
+  const [elementIsVisibe, setElementIsVisible] = useState(false);
+  const bg = {
+    true: {
+      left: "7rem",
+    },
+    false: {
+      left: "19rem",
+    },
+  };
+  const musicPlayer = {
+    true: {
+      left: "295px",
+    },
+    false: {
+      left: "235px",
+    },
+  };
+  const rect = {
+    true: {
+      left: "11rem",
+    },
+    false: {
+      left: "13rem",
+    },
+  };
+  const heart = {
+    true: {
+      left: "9rem",
+    },
+    false: {
+      left: "12.5rem",
+    },
+  };
   return (
-    <div className="wrapper bg-[#081730] flex items-center justify-between px-[5rem] rounded-b-[5rem] w-[100%] h-[35rem] relative z-[3]">
-      {/* left side */}
-      <div className="headings flex flex-col items-start justify-center h-[100%] text-[3rem]">
-        <span>Experience The</span>
-        <span>
-          <b>Best Quality Music</b>
-        </span>
-        <span className="text-[15px] text-[#525D6E]">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
-          <br />
-          Lorem Ipsum has been the industry's standard dummy text ever since the
-          1500s
-        </span>
-        {/* download ads */}
-        <div>
-          <span className="text-[13px]">Download now in IOS and Android</span>
-          <DownloadAds />
+    <VisibilitySensor
+      onChange={(isVisible) => setElementIsVisible(isVisible)}
+      minTopValue={300}
+    >
+      <div className="wrapper bg-[#081730] flex items-center justify-between px-[5rem] rounded-b-[5rem] w-[100%] h-[35rem] relative z-[3]">
+        {/* left side */}
+        <div className="headings flex flex-col items-start justify-center h-[100%] text-[3rem]">
+          <span>Experience The</span>
+          <span>
+            <b>Best Quality Music</b>
+          </span>
+          <span className="text-[15px] text-[#525D6E]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+            <br />
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s
+          </span>
+          {/* download ads */}
+          <div>
+            <span className="text-[13px]">Download now in IOS and Android</span>
+            <DownloadAds />
+          </div>
+        </div>
+        {/* right side */}
+        <div className="images relative w-[50%]">
+          <motion.img
+            variants={bg}
+            animate={`${elementIsVisibe}`}
+            transition={{ duration: 1, type: "ease-out" }}
+            src={require("../img/backgraphics.png")}
+            alt=""
+            className="absolute top-[-8rem] left-[19rem]"
+          />
+          <img
+            transition={{ duration: 1, type: "ease-out" }}
+            src={require("../img/p 1.png")}
+            alt=""
+            className="absolute top-[-15rem] h-[34rem] left-[13rem]"
+          />
+          <motion.img
+            variants={musicPlayer}
+            animate={`${elementIsVisibe}`}
+            transition={{ duration: 1, type: "ease-out" }}
+            mg
+            src={require("../img/p 2.png")}
+            alt=""
+            className="absolute left-[235px] top-[94px] w-[175px]"
+          />
+          <motion.img
+            variants={rect}
+            animate={`${elementIsVisibe}`}
+            transition={{ duration: 1, type: "ease-out" }}
+            src={require("../img/p 3.png")}
+            alt=""
+            className="absolute w-[5rem] left-[13rem] top-[12rem]"
+          />
+          <motion.img
+            variants={heart}
+            animate={`${elementIsVisibe}`}
+            transition={{ duration: 1, type: "ease-out" }}
+            src={require("../img/p 4.png")}
+            alt=""
+            className="absolute w-[5rem] left-[12.5rem] top-[12rem]"
+          />
         </div>
       </div>
-      {/* right side */}
-      <div className="images relative w-[50%]">
-        <img
-          src={require("../img/backgraphics.png")}
-          alt=""
-          className="absolute top-[-8rem] left-[19rem]"
-        />
-        <img
-          src={require("../img/p 1.png")}
-          alt=""
-          className="absolute top-[-15rem] h-[34rem] left-[13rem]"
-        />
-        <img
-          src={require("../img/p 2.png")}
-          alt=""
-          className="absolute left-[235px] top-[94px] w-[175px]"
-        />
-        <img
-          src={require("../img/p 3.png")}
-          alt=""
-          className="absolute w-[5rem] left-[13rem] top-[12rem]"
-        />
-        <img
-          src={require("../img/p 4.png")}
-          alt=""
-          className="absolute w-[5rem] left-[12.5rem] top-[12rem]"
-        />
-      </div>
-    </div>
+    </VisibilitySensor>
   );
 }
 
